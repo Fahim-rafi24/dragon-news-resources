12b40445f6edfb7a4842402c91321dff2b95cdeb
Fahim Rafi <fahimrafibd2003@gmail.com>
2024-10-22 18:20:30 +0600

    create all News Card

--- src/components/LeftBar/LeftBar.jsx
+++ src/components/LeftBar/LeftBar.jsx
@@ -1,15 +1,17 @@
 import { Link, NavLink } from "react-router-dom";
-
+import categories from "../../assets/data/categories.json"
 
 
 export default function LeftBar() {
+
+    // console.log(categories)
 
 
     return (
         <div className="">
             <h2 className="text-center text-3xl my-4">All Caterogy</h2>
             <ul>
-                <li className={`hover:bg-gray-400 hover:text-white py-4 pl-10`}><NavLink>National News</NavLink></li>
+                {/* <li className={`hover:bg-gray-400 hover:text-white py-4 pl-10`}><NavLink>National News</NavLink></li>
                 <li className={`hover:bg-gray-400 hover:text-white py-4 pl-10`}><Link>All News</Link></li>
                 <li className={`hover:bg-gray-400 hover:text-white py-4 pl-10`}><Link>Breaking News</Link></li>
                 <li className={`hover:bg-gray-400 hover:text-white py-4 pl-10`}><Link>Regular News</Link></li>
@@ -17,7 +19,12 @@ export default function LeftBar() {
                 <li className={`hover:bg-gray-400 hover:text-white py-4 pl-10`}><Link>Sports</Link></li>
                 <li className={`hover:bg-gray-400 hover:text-white py-4 pl-10`}><Link>Entertainment</Link></li>
                 <li className={`hover:bg-gray-400 hover:text-white py-4 pl-10`}><Link>Culture</Link></li>
-                <li className={`hover:bg-gray-400 hover:text-white py-4 pl-10`}><Link>Arts</Link></li>
+                <li className={`hover:bg-gray-400 hover:text-white py-4 pl-10`}><Link>Arts</Link></li> */}
+                {categories.map(category => (
+                    <li key={category.id} className="hover:bg-gray-400 hover:text-white py-4 pl-10">
+                        <Link>{category.name}</Link>
+                    </li>
+                ))}
             </ul>
         </div>
     )
--- src/components/NewsBar/NewsBar.jsx
+++ src/components/NewsBar/NewsBar.jsx
@@ -1,10 +1,66 @@
+// json data load 
+import newsAll from '../../assets/data/news.json'
+
+// common img import
+import Eye from '../../assets/img/Eye.png'
+import Bookmark from '../../assets/img/BookMark.png'
+import Share from '../../assets/img/Shaired.png'
+import Rating from '../../assets/img/Rating.png'
 
 
 
-export default function NewsBar(){
-    return(
-        <>
-        
-        </>
+export default function NewsBar() {
+    console.log(newsAll[0])
+    // header
+    // const { img, name, published_date } = newsAll[0].author;
+    // const { number } = newsAll[0].rating;
+    // const { title, image_url, details, total_view } = newsAll[0];
+    return (
+        <div className='min-h-full'>
+            <h2 className='pl-5 pt-10 text-xl font-semibold text-gray-600'>Dragon News Home</h2>
+
+
+
+            {/* map All card from json */}
+            {newsAll.map(card => (
+                // each card
+                <div className="card my-9 border rounded-lg">
+                    {/* card Header */}
+                    <div className="top bg-gray-100 h-20 flex justify-between rounded-t-lg p-3">
+                        <section className='flex'>
+                            <div className='h-12 w-12'>
+                                <img src={card.author.img} className='rounded-full' />
+                            </div>
+                            <div className='ml-3 text-sm'>
+                                <h5 className='font-bold'>{card.author.name}</h5>
+                                <p>{card.author.published_date}</p>
+                            </div>
+                        </section>
+                        <section className='flex mt-3'>
+                            <div className='w-7 h-7 mr-3'>
+                                <img src={Bookmark} alt="" />
+                            </div>
+                            <div className='w-7 h-7'>
+                                <img src={Share} alt="" />
+                            </div>
+                        </section>
+                    </div>
+                    {/* card Title */}
+                    <h2 className='text-2xl font-semibold p-3 text-gray-700'>{card.title}</h2>
+                    <img className='m-3' src={card.image_url} alt="" />
+                    <p className='p-3'>{card.details.substring(0, 200)}...</p>
+                    <p className='pl-3 text-orange-500 font-bold'>Read More</p>
+                    <hr className='mx-3 my-5' />
+                    <section className='flex justify-between mb-5'>
+                        <div className='px-3 flex'><img src={Rating} className='mr-3' /> {card.rating.number}</div>
+                        <div className='px-3 flex'><img src={Eye} className='mr-3' /> {card.total_view}</div>
+                    </section>
+                </div>
+                // each Card end
+            ))}
+
+
+
+        </div>
     )
 }
