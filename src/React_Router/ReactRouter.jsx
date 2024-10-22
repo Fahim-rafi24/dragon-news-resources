e40fdf75bc610c11b4793b44448a80e63cf5fa71
Fahim Rafi <fahimrafibd2003@gmail.com>
2024-10-22 13:16:22 +0600

    add DaisyUi

--- src/React_Router/ReactRouter.jsx
+++ src/React_Router/ReactRouter.jsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "../Root";
 
 
 
@@ -7,7 +8,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <div>Hello world!</div>,
+        element: <Root></Root>,
     },
 ]);
 
---
+++ src/Root.jsx
@@ -0,0 +1,12 @@
+
+
+
+export default function Root(){
+    return(
+        <>
+        {/* Header */}
+        {/* Main Body */}
+        {/* Footer */}
+        </>
+    )
+}
